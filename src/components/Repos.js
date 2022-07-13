6601fb830b0edb4241857f304eb70c9ee34aeb91
SziNo <szino1848@gmail.com>
2022-07-13 21:22:06 +0200

    initial commit

---
+++ src/components/Repos.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import styled from 'styled-components'
+import { GithubContext } from '../context/context'
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
+
+const Repos = () => {
+  const { repos } = React.useContext(GithubContext)
+  console.log(repos)
+  const languages = repos.reduce((acc, cur) => {
+    const { language, stargazers_count } = cur
+    if (!language) return acc
+    acc[language]
+      ? (acc[language] = {
+          ...acc[language],
+          value: acc[language].value + 1,
+          stars: acc[language].stars + 1,
+        })
+      : (acc[language] = {
+          label: language,
+          value: 1,
+          stars: stargazers_count,
+        })
+    return acc
+  }, {})
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => b.value - a.value)
+    .slice(0, 5)
+
+  // most stars per language
+
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => b.stars - a.stars)
+    .map((item) => {
+      return { ...item, value: item.stars }
+    })
+    .slice(0, 5)
+
+  // stars, forks
+
+  let { stars, forks } = repos.reduce(
+    (acc, cur) => {
+      const { stargazers_count, name, forks } = cur
+      acc.stars[stargazers_count] = { label: name, value: stargazers_count }
+      acc.forks[forks] = { label: name, value: forks }
+      return acc
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  )
+
+  stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <Pie3D data={mostUsed} />
+        <Column3D data={stars} />
+        <Doughnut2D data={mostPopular} />
+        <Bar3D data={forks} />
+      </Wrapper>
+    </section>
+  )
+}
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`
+
+export default Repos
